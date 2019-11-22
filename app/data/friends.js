96b1857d2235016b2233246dcd6002f6b15b3120
Leah Munson <munsonleahr@gmail.com>
2019-11-21 17:48:20 -0800

    added data in friends array: schitts creek theme

---
+++ app/data/friends.js
@@ -0,0 +1,42 @@
+var friendsData = [
+    {
+        name: "Alexis Rose",
+        photo:"https://vignette.wikia.nocookie.net/schitts-creek/images/1/10/Alexis-s5.jpg/revision/latest?cb=20190111053312",
+        scores: [3, 3, 2, 4, 1, 1, 2, 5, 4, 2]
+    },
+    {
+        name: "Stevie Budd",
+        photo:"https://www.cbc.ca/schittscreek/content/images/people/stevie-s5.jpg",
+        scores: [5, 5, 2, 1, 3, 2, 1, 1, 4, 4]
+    },
+    {
+        name: "David Rose",
+        photo:"https://vignette.wikia.nocookie.net/schitts-creek/images/6/62/David-s5.jpg/revision/latest?cb=20190111053100",
+        scores: [1, 2, 5, 3, 4, 1, 5, 2, 4, 3]
+    },
+    {
+        name: "Moira Rose",
+        photo:"https://d2l4kn3pfhqw69.cloudfront.net/wp-content/uploads/2019/02/arts-pro-catherine-1024x684.jpg",
+        scores: [4, 4, 3, 2, 2, 1, 5, 2, 3, 1]
+    },
+    {
+        name: "Twyla Sands",
+        photo:"https://www.cbc.ca/schittscreek/content/images/people/twyla-s4.jpg",
+        scores: [2, 3, 2, 5, 2, 1, 5, 5, 4, 3]
+      },
+    {
+        name: "Roland Schitt",
+        photo:"https://www.cbc.ca/schittscreek/content/images/people/roland-s4.jpg",
+        scores: [3, 3, 1, 2, 4, 5, 4, 4, 3, 2]
+    },
+    {
+      name: "Ted Mullens",
+      photo:"https://vignette.wikia.nocookie.net/schitts-creek/images/b/b9/Ted-mullens-s2.jpg/revision/latest?cb=20170519213518",
+      scores: [4, 5, 4, 1, 3, 1, 2, 4, 1, 5]
+    }
+  ];
+  
+  
+  
+  // Export to API Route
+  module.exports = friendsData;
